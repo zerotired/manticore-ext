ce95229e2fd32aaa8f9cd70196d11b5a2ccc81a6
Andreas Motl <a.motl@zerotired.com>
2014-04-19 20:45:39 +0200

    version bump: 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name='zt.manticore.ext',
-    version='0.1.5',
+    version='0.2.0',
     url='https://github.com/zerotired/manticore-ext',
     download_url='',
     license='BSD',
